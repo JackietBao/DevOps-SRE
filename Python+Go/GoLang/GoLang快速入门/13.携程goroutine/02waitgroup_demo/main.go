2cf1ffd6e717b33aa67496c397ac576959157859
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-03-19 20:52:22 +0800

    Update main.go

--- Python+Go/GoLang/GoLang快速入门/13.携程goroutine/02waitgroup_demo/main.go
+++ Python+Go/GoLang/GoLang快速入门/13.携程goroutine/02waitgroup_demo/main.go
@@ -7,6 +7,9 @@ import (
 )
 
 var wg sync.WaitGroup // 第一步：定义一个计数器
+//定义了一个全局变量 wg
+//是一个 sync.WaitGroup 类型的变量
+//用于等待一组 Goroutines 完成任务
 func test1() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("test1() 你好golang-", i)
@@ -22,11 +25,16 @@ func test2() {
 	wg.Done() //协程计数器-1
 }
 func main() {
-	wg.Add(1)  //协程计数器+1 第二步：开启一个协程计数器+1
+	wg.Add(1) //协程计数器+1 第二步：开启一个协程计数器+1
+	//增加了 WaitGroup 的计数器，将要开启的 Goroutines 数量加 1
 	go test1() //表示开启一个协程
-	wg.Add(1)  //协程计数器+1
+	//开启一个新的 Goroutine 来执行 test1() 函数
+	wg.Add(1) //协程计数器+1
+	//再次增加了 WaitGroup 的计数器，又要开启的 Goroutines 数量加 1。
 	go test2() //表示开启一个协程
-	wg.Wait()  //等待协程执行完毕... 第四步：计数器为0时推出
+	//开启另一个 Goroutine 来执行 test2() 函数
+	wg.Wait() //等待协程执行完毕... 第四步：计数器为0时推出
+	//等待所有 Goroutines 完成任务，即等待 WaitGroup 的计数器归零。在这之后，程序将继续执行后面的代码
 	fmt.Println("主线程退出...")
 }
 
