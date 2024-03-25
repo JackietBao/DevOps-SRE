d0c285e38fd543af6cb2bc5bc40ef7ca6a0a61f1
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-03-25 16:29:14 +0800

    Update main.go

--- Python+Go/GoLang/GoLang快速入门/9.条件循环/04break_and_continue/main.go
+++ Python+Go/GoLang/GoLang快速入门/9.条件循环/04break_and_continue/main.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 	// break: 跳出当前循环（循环结束）
 	// continue: 结束当前这一次循环，进行下一次循环
+
 	for i := 1; i < 10; i++ {
 		if i == 5 {
-			//break
+			//break // 直接跳出了for循环
 			continue
 		}
 		fmt.Println(i)
