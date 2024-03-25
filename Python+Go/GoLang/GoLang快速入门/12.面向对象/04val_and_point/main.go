8e84af8e1a078a9e940629369dd86b2590b5f403
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-03-25 16:28:45 +0800

    Update main.go

--- Python+Go/GoLang/GoLang快速入门/12.面向对象/04val_and_point/main.go
+++ Python+Go/GoLang/GoLang快速入门/12.面向对象/04val_and_point/main.go
@@ -3,30 +3,29 @@ package main
 import "fmt"
 
 type Usb interface {
-	//定义了一个接口 Usb，该接口包含了 Start() 和 Stop() 两个方法
 	Start()
 	Stop()
 }
 type Phone struct {
-	//结构体 Phone，包含了一个字段 Name，代表手机的名称
 	Name string
 }
 
-func (p Phone) Start() {
+// 用了指针，只要任意一个地方修改，其他所有地方都会改是吧
+func (p *Phone) Start() {
 	fmt.Println(p.Name, "开始工作")
 }
 func (p Phone) Stop() {
 	fmt.Println("phone 停止")
 }
-
-//Phone 结构体上定义了 Start() 和 Stop() 两个方法，以满足 Usb 接口的实现要求
 func main() {
-	phone1 := Phone{ // 一：实例化值类型
-		Name: "小米手机",
-	}
-	var p1 Usb = phone1 //phone1 实现了 Usb 接口 phone1 是 Phone 类型
-	//将 phone1 赋值给 Usb 类型的变量 p1
-	p1.Start()
+	//phone1 := Phone{ // 一：实例化值类型
+	//	Name: "小米手机",
+	//}
+	//var p1 Usb = phone1 //phone1 实现了 Usb 接口 phone1 是 Phone 类型
+	//p1.Start()
+
+	// 如果接口中使用的是指针类型作为接收者，那么只能使用指针类型的结构体去调用
+	// 不能使用值类型的结构体调用
 	phone2 := &Phone{ // 二：实例化指针类型
 		Name: "苹果手机",
 	}
