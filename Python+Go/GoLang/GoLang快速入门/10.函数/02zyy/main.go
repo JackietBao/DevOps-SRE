ae5c896b87082f57c86e0eacbc87761235d45f6b
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-03-25 16:28:35 +0800

    Update main.go

--- Python+Go/GoLang/GoLang快速入门/10.函数/02zyy/main.go
+++ Python+Go/GoLang/GoLang快速入门/10.函数/02zyy/main.go
@@ -1,45 +1,43 @@
+package main
+
+import "fmt"
+
+// go语言执行顺序
 /*
-
-全局变量
-package main
-
-import "fmt"
-
-//定义全局变量 num
-var num int64 = 10
-
-func main() {
-	fmt.Printf("num=%d\n", num) //num=10
-}
-//局部变量
-package main
-
-import "fmt"
-
-func main() {
-	// 这里name是函数test的局部变量，在其他函数无法访问
-	//fmt.Println(name)
-	test()
-}
-func test() {
-	name := "zhangsan"
-	fmt.Println(name)
-}
-
-//for 循环语句中定义的变量
-
+首先执行 定义全局变量
+然后执行 init方法
+最后执行 main方法，调用我们写的逻辑
 */
 
-package main
-
-import "fmt"
+// 1、全局变量
+// 在函数外部直接定义的变量是全局变量（特点：可以在其他函数中使用）
+var num int = 64
 
 func main() {
-	testLocalVar3()
+	TestFunc()
+	fmt.Println("main", num) // 这里打印的是全局的num
+	//fmt.Println(num2)  // 局部变量在函数内部生效，不能在其他函数调用
 }
-func testLocalVar3() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(i) //变量 i 只在当前 for 语句块中生效
-	}
-	// fmt.Println(i) //此处无法使用变量 i
+
+func TestFunc() {
+	//fmt.Println(num) // 全局变量可以在其他函数中使用
+	//// 2、局部变量（在函数内部定义的变量，不能在其他函数中访问）
+	//var num2 int = 1000
+	//fmt.Println(num2)
+	//
+	//// 3、for循环中定义的变量
+	//for i := 1; i < 10; i++ {
+	//	fmt.Println(i)
+	//}
+	////fmt.Println(i)  // 在for后面定义的变量是自己这个代码块的，在外部不能使用
+
+	//fmt.Println("test", num) // 全局变量可以在其他函数中使用
+	//num = 3000
+
+	// 局部作用域优先级更高，局部和全局定义了相同的变量名，局部在函数内部生效的，全局的被覆盖了
+	num := 7 // := 类似var 声明一个新的变量(在内存中开启新空间)     = 是赋值
+	fmt.Println("test", num)
+	num = 444 // 函数内部如果修改，只修改的是自己，不影响全局的num
+	fmt.Println("test", num)
+
 }
