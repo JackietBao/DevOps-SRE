dd75d9cba3b3fc2b7b1dbee5853e38424aa8eb31
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-03-25 16:28:47 +0800

    Update main.go

--- Python+Go/GoLang/GoLang快速入门/13.携程goroutine/02waitgroup_demo/main.go
+++ Python+Go/GoLang/GoLang快速入门/13.携程goroutine/02waitgroup_demo/main.go
@@ -6,43 +6,34 @@ import (
 	"time"
 )
 
+/*
+// 计数器怎么知道有一共有多少次的
+goroutine开启的时候进行 wg.Add(1) 加一
+goroutine结束是进行 wg.Done() 加1
+wg.Wait() 会判断当前的goroutine是否为0，为0就退出
+
+var wg sync.WaitGroup       // 第一步：定义一个计数器
+wg.Add(1)               // 第二步：开启一个协程计数器+1
+wg.Done()               // 第三步：协程执行完毕，计数器-1
+wg.Wait()               // 第四步：计数器为0时推出
+*/
+
 var wg sync.WaitGroup // 第一步：定义一个计数器
-//定义了一个全局变量 wg
-//是一个 sync.WaitGroup 类型的变量
-//用于等待一组 Goroutines 完成任务
-func test1() {
-	for i := 0; i < 10; i++ {
-		fmt.Println("test1() 你好golang-", i)
-		time.Sleep(time.Millisecond * 100)
-	}
-	wg.Done() //协程计数器-1 // 第三步：协程执行完毕，计数器-1
-}
-func test2() {
-	for i := 0; i < 2; i++ {
-		fmt.Println("test2() 你好golang-", i)
-		time.Sleep(time.Millisecond * 100)
-	}
-	wg.Done() //协程计数器-1
-}
+
 func main() {
-	wg.Add(1) //协程计数器+1 第二步：开启一个协程计数器+1
-	//增加了 WaitGroup 的计数器，将要开启的 Goroutines 数量加 1
-	go test1() //表示开启一个协程
-	//开启一个新的 Goroutine 来执行 test1() 函数
-	wg.Add(1) //协程计数器+1
-	//再次增加了 WaitGroup 的计数器，又要开启的 Goroutines 数量加 1。
-	go test2() //表示开启一个协程
-	//开启另一个 Goroutine 来执行 test2() 函数
-	wg.Wait() //等待协程执行完毕... 第四步：计数器为0时推出
-	//等待所有 Goroutines 完成任务，即等待 WaitGroup 的计数器归零。在这之后，程序将继续执行后面的代码
-	fmt.Println("主线程退出...")
+	wg.Add(2) // 第二步：开启一个协程计数器+1
+	go test()
+	go test()
+	// wg.Wait()发现没有goroutine在使用而我们的wg不为0，那么就会触发异常
+	wg.Wait() // 第四步：计数器为0时推出
+	//time.Sleep(time.Second)
+	fmt.Println("main over")
 }
 
-/*
-test2() 你好golang- 0
-test1() 你好golang- 0
-.....
-test1() 你好golang- 8
-test1() 你好golang- 9
-主线程退出...
-*/
+func test() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		time.Sleep(100 * time.Microsecond)
+	}
+	wg.Done() // 第三步：协程执行完毕，计数器-1
+}
