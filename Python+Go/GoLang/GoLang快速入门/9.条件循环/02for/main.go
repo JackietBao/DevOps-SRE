0de1c874c7c94c6ab76636de69a021f4d18bbaf0
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-03-25 16:29:12 +0800

    Update main.go

--- Python+Go/GoLang/GoLang快速入门/9.条件循环/02for/main.go
+++ Python+Go/GoLang/GoLang快速入门/9.条件循环/02for/main.go
@@ -3,36 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	/*
-		// 1、输出1到100
-		for i := 1; i <= 100; i++ {
-			fmt.Println(i)
-		}
-	*/
+	//// 1、输出1到100
+	///*
+	//i := 1  赋值语句
+	//i <= 100  条件
+	//i++   i = i + 1
+	// */
+	//for i := 1; i <= 100; i++ {
+	//	fmt.Println(i)
+	//}
+	//// 2、将i定义到外部
+	//i := 0
+	//for i < 10 { // 容易出现死循环，一定要有结束的方法
+	//	fmt.Println(i)
+	//	i++ // i = i + 1 (不是从0开始打印，而是从 i = 0 + 1)
+	//	// i++在fmt上面打印 1~10
+	//	// i++在fmt下面打印 0~9
+	//}
 
-	//2、将i定义到外部
-	i := 0
-	for i < 10 {
-		fmt.Println(i)
-		i++
-	}
-	// 3、for模拟while循环 （while true）
-	k := 1
-	for {
-		if k <= 10 {
-			fmt.Println(k)
-		} else {
-			break
-		}
-		k++
-	}
+	//// 3、for模拟while循环 （while true）
+	//k := 1
+	//for {
+	//	if k <= 10 {
+	//		fmt.Println(k)
+	//	} else {
+	//		break
+	//	}
+	//	k++
+	//}
+
 	// 4、for range打印索引和值
 	s := "hello world"
 	for index, val := range s {
-		//for 循环，通过 range 关键字遍历字符串 s 中的每个字符
-		//每次迭代中，index 表示当前字符在字符串中的索引
-		//val 则表示当前字符的Unicode码点值
 		fmt.Println(index, val)
-
 	}
 }
