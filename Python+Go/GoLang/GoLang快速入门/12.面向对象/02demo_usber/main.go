4ca0906cbbc34bafd95e63a83a89908bda724fa2
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-03-25 16:28:43 +0800

    Update main.go

--- Python+Go/GoLang/GoLang快速入门/12.面向对象/02demo_usber/main.go
+++ Python+Go/GoLang/GoLang快速入门/12.面向对象/02demo_usber/main.go
@@ -2,39 +2,90 @@ package main
 
 import "fmt"
 
-//1.接口是一个规范
-type Usber interface {
-	//Usber 是一个接口类型，它规定了两个方法：start() 和 stop()
-	//接口的目的是描述一种行为规范，任何实现了这两个方法的类型都可以被视为 Usber 接口的实现者
-	start()
-	stop()
-}
+/*
+interface这个接口是golang中的一个数据类型（int、string）
+和框架写的api接口没有任何关系，只是名字相同而已
+*/
 
-//2.如果接口里面有方法的话，必要要通过结构体或者通过自定义类型实现这个接口
-type Phone struct {
-	//Phone 结构体表示手机，拥有一个字段 Name，表示手机的名字
-	Name string
-}
-
-//3.手机要实现usb接口的话必须得实现usb接口中的所有方法
-func (p Phone) start() {
-	fmt.Println(p.Name, "启动")
-}
-func (p Phone) stop() {
-	fmt.Println(p.Name, "关机")
-}
+/*
+1.函数编程：每个函数之间没有任何关系
+2.结构体绑定方法：实现一类事物通用的方法，这些方法是有关系
+定义一个人：eat方法、say方法 这些方法是有关系，都是定义了一个人能做的事情
+*/
 func main() {
+	// 第四步：初始化phone结构体
 	p := Phone{
-		Name: "华为手机",
+		Name: "Mate 30 pro",
 	}
-	var p1 Usber // golang中接口就是一个数据类型
-	//声明了一个变量 p1，类型为 Usber
-	p1 = p // 表示手机实现Usb接口
-	p1.start()
-	p1.stop()
+
+	// 第五步：通过接口来调用方法
+	var p1 Usber
+	p1 = p
+	Run(p1)
+	//p1.Start()
+	//p1.Stop()
+	//
+	//c := Computer{
+	//	Name: "苹果",
+	//}
+	//var c1 Usber
+	//c1 =
+	//	Run(p1)
+	//c.Start()
+
 }
 
 /*
-华为手机 启动
-华为手机 关机
+第一句话：接口（interface）定义了一个对象的行为规范，
+`只定义规范不实现`，由具体的`对象来实现规范的细节`
+第二句话：一个对象只要全部实现了接口中的方法，那么就实现了这个接口。
 */
+// 第一步：定义一个接口 , 接口的名字 xxxer结尾，只是一个规范
+/*
+Usber  是接口的名字
+interface  定义接口的关键字
+*/
+type Usber interface {
+	// type Usber interface 就是定义了一个接口
+	// Start() / Stop() 定义必须要实现这两个方法（对象实现的细节）
+	Start()
+	Stop()
+}
+
+/* 应为 Phone没有实现Usber要求实现的规范：Stop、Start两个方法
+Cannot use 'p' (type Phone) as the type Usber
+Type does not implement 'Usber' as some methods are missing: Start()
+*/
+
+// 第二句话：一个对象只要全部实现了接口中的方法，那么就实现了这个接口。
+// 第二步：定义一个结构体
+type Phone struct {
+	Name string
+}
+
+// 第三步：Phone这个结构体实现了接口中定义的两个方法 Start、Stop
+func (p Phone) Start() {
+	fmt.Println("start ...")
+}
+
+func (p Phone) Stop() {
+	fmt.Println("stop ...")
+}
+
+type Computer struct {
+	Name string
+}
+
+// 第三步：Phone这个结构体实现了接口中定义的两个方法 Start、Stop
+func (c Computer) Start() {
+	fmt.Println("start ...")
+}
+
+func (c Computer) Stop() {
+	fmt.Println("stop ...")
+}
+
+func Run(u Usber) {
+	u.Stop()
+	u.Start()
+}
