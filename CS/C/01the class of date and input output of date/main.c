0fd2951f1168404553e6b978e59048b9d174a509
JackietBao <125871364+JackietBao@users.noreply.github.com>
2024-04-14 00:41:20 +0800

    CS

---
+++ CS/C/01the class of date and input output of date/main.c
@@ -0,0 +1,22 @@
+
+//符号常量
+
+/*
+#include <stdio.h>
+#define PI 3+2
+int main() {
+    int i=PI*2;
+    printf("i=%d\n",i);
+    return 0;
+}
+ */
+
+
+
+#include <stdio.h>
+int main() {
+    char c='A';
+    printf("%c\n",c+32);
+    printf("%d\n",c);
+    return 0;
+}
